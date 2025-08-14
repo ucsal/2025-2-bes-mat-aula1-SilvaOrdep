29100bcd22b19314be963fc0545d3d43ec642ef2
PEDRO HENRIQUE OLIVEIRA SILVA <200033264@ucsal.br>
2025-08-14 08:57:27 -0300

    feat: implementação da classe abstrada User

--- src/main/java/cms/user/User.java
+++ src/main/java/cms/user/User.java
@@ -1,5 +1,31 @@
 package cms.user;
 
 public abstract class User {
+	private Long id;
+	private String nome;
+	private String email;
+	private String password;
 
+	public User(Long id, String nome, String email, String password) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public boolean checkPassword(String p) {
+		return password.equals(p);
+	}
 }
